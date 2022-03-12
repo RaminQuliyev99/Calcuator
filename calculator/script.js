c1f606c9e595f06522797dc30e594389dd05dc82
RaminQuliyev99 <98511186+RaminQuliyev99@users.noreply.github.com>
2022-03-12 11:01:59 +0400

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,51 @@
+let inp1 = document.getElementById("inp");
+let opt = document.getElementById("operator");
+
+
+
+
+function reqemler1() {
+    inp1.value += '1';
+};
+function reqemler2() {
+    inp1.value += '2';
+};
+function reqemler3() {
+    inp1.value += '3';
+};
+function reqemler4() {
+    inp1.value += '4';
+};
+function reqemler5() {
+    inp1.value += '5';
+};
+function reqemler6() {
+    inp1.value += '6';
+};
+function reqemler7() {
+    inp1.value += '7';
+};
+function reqemler8() {
+    inp1.value += '8';
+};
+function reqemler9() {
+    inp1.value += '9';
+};
+function reqemler0() {
+    inp1.value += '0';
+};
+function sil(){
+    location.reload;
+};
+function operatorpl(){
+    inp1.value += '+'
+};
+function operatormn(){
+    inp1.value += '-'
+};
+function operatorvr(){
+    inp1.value += '*'
+};
+function operatorbl(){
+    inp1.value += '/'
+};
